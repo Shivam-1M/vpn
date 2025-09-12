23bb33f1c505433c0d465d78ef307222bab4a862
Shivam-1M <shivammungra39@gmail.com>
2025-09-12 06:00:36 -0400

    implemented asynchronous TUN virtual network interface

--- vpn_data_plane/src/main.rs
+++ vpn_data_plane/src/main.rs
@@ -4,6 +4,7 @@
 //! at high speed.
 
 use std::error::Error;
+use tokio_tun::TunBuilder;
 
 // This is the main entry point for our asynchronous application.
 // The #[tokio::main] attribute transforms the async main function
@@ -16,10 +17,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     tracing::info!("Initializing VPN Data Plane...");
 
-    // --- TODO: Step 1 ---
-    // Create an asynchronous TUN interface. This will be our virtual network card
-    // that the operating system sees. All traffic for the VPN will go through this.
-    // We will use the `tokio-tun` crate for this.
+    // --- Step 1: Create an asynchronous TUN interface ---
+    // This creates a new virtual network interface. We give it a name,
+    // set the IP address and netmask for our side of the tunnel, and bring it up.
+    // On Linux, this will create a device like 'tun0'. You'll need to run this
+    // with `sudo` for it to have the necessary permissions to create the interface.
+    let tun = TunBuilder::new()
+        .name("wg0") // Interface name
+        .up() // Bring the interface up
+        .build()?;
+
+    let tun = tun.into_iter().next().unwrap();
+    
+    tracing::info!("Successfully created TUN device: {}", tun.name());
 
     // --- TODO: Step 2 ---
     // Initialize and configure the WireGuard backend using `defguard_wireguard_rs`.
@@ -30,9 +40,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // where we will read packets from the TUN device, pass them to WireGuard for
     // encryption/decryption, and send them out to the internet (and vice-versa).
 
-    tracing::info!("VPN Data Plane started successfully. (Placeholder)");
+    tracing::info!("VPN Data Plane is running. (TUN device created)");
 
-    // For now, the application just prints a message and exits.
-    // In the future, this will be an infinite loop that runs the VPN service.
+    // The application will now wait indefinitely. We will replace this later
+    // with the main packet processing loop.
+    tokio::time::sleep(tokio::time::Duration::from_secs(u64::MAX)).await;
+
     Ok(())
 }
